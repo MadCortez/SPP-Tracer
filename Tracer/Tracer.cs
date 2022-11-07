49f8b2fcf2ca94bd67ed086779b22533f99b75d3
Илья <@DESKTOP-GEAJTR5>
2022-11-08 01:12:18 +0300

    Добавьте файлы проекта.

---
+++ Tracer/Tracer.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace TracerApp {
+    public class TraceResult {
+        public Stopwatch stopwatch;
+        public long time { get; set; }
+        public string methodName { get; set; }
+        public string className { get; set; }
+        public List<TraceResult> traceResultList { get; set; }
+    }
+
+    public interface ITracer {
+        void StartTrace();
+        void StopTrace();
+        string[] GetTraceResult();
+    }
+
+    public class Tracer : ITracer {
+        ConcurrentDictionary<int, Stack<TraceResult>> threadsResults;
+        public Tracer() {
+            threadsResults = new ConcurrentDictionary<int, Stack<TraceResult>>();
+        }
+
+        public void StartTrace() {
+            TraceResult traceResult = new TraceResult();
+            Stack<TraceResult> stackTraceResult = new Stack<TraceResult>();
+            StackTrace stackTrace = new StackTrace();
+            StackFrame frame = stackTrace.GetFrame(1);
+            MethodBase method = frame.GetMethod();
+            Type type = method.DeclaringType;
+            traceResult.time = 0;
+            traceResult.methodName = method.Name;
+            traceResult.className = type.Name;
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            traceResult.stopwatch = stopwatch;
+            traceResult.traceResultList = new List<TraceResult>();
+            threadsResults.GetOrAdd(Thread.CurrentThread.ManagedThreadId, _ => new Stack<TraceResult>());
+            threadsResults.TryGetValue(Thread.CurrentThread.ManagedThreadId, out stackTraceResult);
+            if (stackTraceResult.Count == 0) {
+                TraceResult root = new TraceResult();
+                root.methodName = Thread.CurrentThread.ManagedThreadId.ToString();
+                root.className = "Thread";
+                root.time = 0;
+                Stopwatch stopwatch1 = new Stopwatch();
+                stopwatch1.Start();
+                root.stopwatch = stopwatch1;
+
+                root.traceResultList = new List<TraceResult>();
+
+                stackTraceResult.Push(root);
+                stackTraceResult.Push(traceResult);
+            }
+            else {
+                stackTraceResult.Push(traceResult);
+            }
+
+            threadsResults.TryAdd(Thread.CurrentThread.ManagedThreadId, stackTraceResult);
+        }
+        public void StopTrace() {
+            Stack<TraceResult> stackTraceResult = new Stack<TraceResult>();
+            threadsResults.TryGetValue(Thread.CurrentThread.ManagedThreadId, out stackTraceResult);
+            TraceResult traceResult = stackTraceResult.Pop();
+            Stopwatch stopwatch = traceResult.stopwatch;
+            stopwatch.Stop();
+            traceResult.time = stopwatch.ElapsedMilliseconds;
+            stackTraceResult.Peek().traceResultList.Add(traceResult);
+            if (stackTraceResult.Count == 1) {
+                Stopwatch stopwatch1 = stackTraceResult.Peek().stopwatch;
+                stopwatch1.Stop();
+                stackTraceResult.Peek().time = stopwatch1.ElapsedMilliseconds;
+                stopwatch1.Start();
+                stackTraceResult.Peek().stopwatch = stopwatch1;
+            }
+            threadsResults.TryAdd(Thread.CurrentThread.ManagedThreadId, stackTraceResult);
+        }
+        public string[] GetTraceResult() {
+            List<TraceResult> TRL = new List<TraceResult>();
+            foreach (KeyValuePair<int, Stack<TraceResult>> item in threadsResults) {
+                TRL.Add(item.Value.Pop());
+                item.Value.Push(TRL[TRL.Count - 1]);
+            }
+            var options = new JsonSerializerOptions {
+                WriteIndented = true,
+            };
+            string json = JsonSerializer.Serialize<List<TraceResult>>(TRL, options);
+            
+            XmlSerializer formatter = new XmlSerializer(typeof(List<TraceResult>));
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            StringWriter textWriter = new StringWriter();
+            using (XmlWriter writer = XmlWriter.Create(textWriter, settings)) {
+                formatter.Serialize(writer, TRL);
+            }
+            string[] result = { json, textWriter.ToString() };
+            return result;
+        }
+    }
+}
---
+++ TracerTests/TracerTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TracerApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TracerApp.Tests {
+    [TestClass()]
+    public class TracerTests {
+        private int waitTime = 100;
+
+        [TestMethod()]
+        public void AnyMethod() {
+            Tracer tracer = new Tracer();
+            tracer.StartTrace();
+            Thread.Sleep(waitTime);
+            tracer.StopTrace();
+        }
+
+        [TestMethod()]
+        public void AnyMethod2() {
+            Tracer tracer = new Tracer();
+            tracer.StartTrace();
+            AnyMethod3();
+            AnyMethod3();
+            tracer.StopTrace();
+        }
+
+        [TestMethod()]
+        public void AnyMethod3() {
+            Tracer tracer = new Tracer();
+            tracer.StartTrace();
+            Thread.Sleep(waitTime / 4);
+            tracer.StopTrace();
+        }
+    }
+}
